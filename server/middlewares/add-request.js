8a1c4e6a80842812e69705ec5d9bd999e9b8b153
Denis <dondiego4697@mail.ru>
2018-11-27 18:52:51 +0300

    wip

--- server/middlewares/add-request.js
+++ server/middlewares/add-request.js
@@ -20,6 +20,9 @@ const smtpTransport = nodeMailer.createTransport({
     }
 });
 
+const writeLog = (text) => {
+    fs.appendFileSync(path.resolve(__dirname, '../../logs/out.log'), `${text}\n`);
+};
 
 const dataSet = new Set();
 setInterval(async () => {
@@ -27,17 +30,18 @@ setInterval(async () => {
         try {
             const url = getStatusUrl(value.id);
             const response = await got.get(url);
-            fs.appendFileSync(path.resolve(__dirname, '../../logs/out.log'), `${value.id} ${response.statusCode} \n`);
+            writeLog(`${value.id} ${response.statusCode}`);
             if (response.statusCode === 200) {
                 const imgPath = path.resolve(__dirname, `../../imgs/${value.id}.png`);
                 got.stream(url).pipe(fs.createWriteStream(imgPath));
-                sendEmail(value.mail, imgPath, value.id)
+                sendEmail(value.mail, imgPath, value.id);
+                writeLog(`${value.mail}, ${imgPath}, ${value.id}`);
                 dataSet.delete(value);
             } else if (response.statusCode === 202 && response.body === 'failed') {
                 dataSet.delete(value);
             }
         } catch (e) {
-            fs.appendFileSync(path.resolve(__dirname, '../../logs/out.log'), `${value.id} ${e} \n`);
+            writeLog(`${value.id} ${e}`);
             dataSet.delete(value);
         }
     });
@@ -66,10 +70,10 @@ const sendEmail = (mail, imgPath, id) => {
             }
         ]
     }, (error, response) => {
-        /* if (error) {
-            console.log(error);
+        if (error) {
+            writeLog(`EMAIL: ${error}`);
         } else {
-            console.log('ok');
-        } */
+            writeLog(`EMAIL: ok send on email ${mail}`);
+        }
     });
 };
